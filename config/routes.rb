e99bbf9ed43f9d72f826ab9438964ff2fb17b206
gurmukhsingh <gurmukhs300@gmail.com>
2022-12-06 17:23:52 +1100

    pg:search

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'movies/index'
   root to: "pages#home"
+  resources :movies, only: :index
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
